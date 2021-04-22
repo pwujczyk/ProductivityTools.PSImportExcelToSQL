018c2dd7b29b09b2190b45b9de64ecd4048fae52
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-04-22 13:33:59 +0200

    rename

--- ProductivityTools.ImportExcelToSQL/Runner.ps1
+++ ProductivityTools.ImportExcelToSQL/Runner.ps1
@@ -2,7 +2,7 @@ clear
 Write-Host "pawel"
 
 cd $PSScriptRoot
-Import-Module d:\GitHub\ProductivityTools.PSExel2SQL\ProductivityTools.PSImportExcelToSQL\\ProductivityTools.PSImportExcelToSQL.psm1 -force
+Import-Module d:\GitHub\ProductivityTools.PSExel2SQL\ProductivityTools.ImportExcelToSQL\\ProductivityTools.ImportExcelToSQL.psm1 -force
 Import-Module "d:\GitHub-3.PublishedToLinkedIn\ProductivityTools.PSSQLCommands\ProductivityTools.PSSQLCommands\ProductivityTools.PSSQLCommands.psm1" -Force
 cd d:\z1\
 Import-ExcelToSql -SqlInstance ".\sql2019" -DatabaseName "EcoVadisDT1" -verbose
